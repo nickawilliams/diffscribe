0ccb6d96c84f035aa1a62506045aa9643ebd082f
Nick Williams <git@nickawilliams.com>
2025-11-12 17:20:41 -0800

    test: added benchmarking

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    "github.com/rogwilco/diffscribe/cmd"
+	"github.com/rogwilco/diffscribe/cmd"
 )
 
 func main() {
-    if err := cmd.Execute(); err != nil {
-        fmt.Fprintln(os.Stderr, err)
-        os.Exit(cmd.ExitCode(err))
-    }
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(cmd.ExitCode(err))
+	}
 }
